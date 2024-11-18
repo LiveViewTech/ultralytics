2f57e0f32416ac3a61a4fee4fca3c633b313f211
Zhengwei Wu <zhengwei.wu@lvt.com>
2024-11-18 09:35:05 -0700

    generate new label cache file everytime to avoid mess

--- ultralytics/data/dataset.py
+++ ultralytics/data/dataset.py
@@ -137,12 +137,12 @@ class YOLODataset(BaseDataset):
         """Returns dictionary of labels for YOLO training."""
         self.label_files = img2label_paths(self.im_files)
         cache_path = Path(self.label_files[0]).parent.with_suffix(".cache")
-        try:
-            cache, exists = load_dataset_cache_file(cache_path), True  # attempt to load a *.cache file
-            assert cache["version"] == DATASET_CACHE_VERSION  # matches current version
-            assert cache["hash"] == get_hash(self.label_files + self.im_files)  # identical hash
-        except (FileNotFoundError, AssertionError, AttributeError):
-            cache, exists = self.cache_labels(cache_path), False  # run cache ops
+        # try:
+        #    cache, exists = load_dataset_cache_file(cache_path), True  # attempt to load a *.cache file
+        #    assert cache["version"] == DATASET_CACHE_VERSION  # matches current version
+        #    assert cache["hash"] == get_hash(self.label_files + self.im_files)  # identical hash
+        #except (FileNotFoundError, AssertionError, AttributeError):
+        cache, exists = self.cache_labels(cache_path), False  # run cache ops
 
         # Display cache
         nf, nm, ne, nc, n = cache.pop("results")  # found, missing, empty, corrupt, total
@@ -307,12 +307,12 @@ class YOLODatasetCSV(YOLODataset):
 
     def get_labels(self):
         cache_path = Path(self.label_files[0]).parent.with_suffix(".cache")
-        try:
-            cache, exists = load_dataset_cache_file(cache_path), True  # attempt to load a *.cache file
-            assert cache["version"] == DATASET_CACHE_VERSION  # matches current version
-            assert cache["hash"] == get_hash(self.label_files + self.im_files)  # identical hash
-        except (FileNotFoundError, AssertionError, AttributeError):
-            cache, exists = self.cache_labels(cache_path), False  # run cache ops
+        # try:
+        #    cache, exists = load_dataset_cache_file(cache_path), True  # attempt to load a *.cache file
+        #    assert cache["version"] == DATASET_CACHE_VERSION  # matches current version
+        #    assert cache["hash"] == get_hash(self.label_files + self.im_files)  # identical hash
+        #except (FileNotFoundError, AssertionError, AttributeError):
+        cache, exists = self.cache_labels(cache_path), False  # run cache ops
 
         # Display cache
         nf, nm, ne, nc, n = cache.pop("results")  # found, missing, empty, corrupt, total
